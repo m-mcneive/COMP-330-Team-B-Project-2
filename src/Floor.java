ce727d7c0bba8c2e6395760b9877799fff01c319
Matt McNeive <matt.mcneive@gmail.com>
2019-12-03 17:59:03 -0600

    Updated floor.java

--- src/Floor.java
+++ src/Floor.java
@@ -1,34 +1,44 @@
 import java.io.*;
 import java.util.*;
 import java.awt.image.BufferedImage;
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.event.*;
 import javax.imageio.ImageIO;
-import javax.swing.*;
 
-
-public class Floor  {
-    public Room[] rooms;
+public class Floor {
+    private Room[] rooms;
    // private ArrayList<Room> roomChange;
-    public int numSpaces; //array Room[] size current count
-    public int level;  // floor level
-    public int size;   //number of rooms 
-    FloorLayout number = new FloorLayout(); 
-   
-    
+    private int numSpaces;
+    private int level;
+    private int size;
+
     public Floor(int size, int level) {
     	this.size = size;
     	rooms = new Room[size];
     	numSpaces = 0;
     	this.level = level;
+    	createEmptyRooms();
     }
-    
-    public void addRoom(Room r) {
+
+    public void createEmptyRooms() {
+
+    	for (int i = 0; i < size; i++) {
+    		rooms[i] = new Room(0, 0, 0, 0, " ");
+    		numSpaces ++;
+    	}
+    }
+
+	public static void readFloorPlan() throws IOException{
+
+	}
+
+	public void addRoom(Room r) {
 		if(numSpaces < size) {
 			rooms[numSpaces] = r;
 			numSpaces++;
 		}
+	}
+
+	public Room getRoom(int roomNum) {
+		return rooms[roomNum - 1];
 	}
 	/*
 	public void removeRoom(Room r, int index) {
@@ -36,31 +46,29 @@ public class Floor  {
 		roomChange = new ArrayList<Room>(Arrays.asList(rooms));
 		roomChange.remove(index);
 		roomChange.toArray(rooms);
-		
+
 		}
 	}
 	*/
-    
-    public int getLevel() {
-    	return level;
-    }
-    
-    public void setUpFloorLayout() {
-    		try {
-    		number.readFloorPlan();
-    		}
-    		catch (IOException e) {
-    			System.out.println("Error in read of Floor.readFloorPlan().");
-    		}
-    	for(int i = 0; i < size; i++) {
-    		Room statusChanger = rooms[i];
-    		number.editFloorPlan(statusChanger, this); // should update the map of the floor based on each room's status	
-    	}
-    	number.printFloorPlan(this);
-    	
-    }
-    
-	
-	
-	
+
+  public int getLevel() {
+     return level;
+   }
+
+   public void setUpFloorLayout() {
+       try {
+       number.readFloorPlan();
+       }
+       catch (IOException e) {
+         System.out.println("Error in read of Floor.readFloorPlan().");
+       }
+     for(int i = 0; i < size; i++) {
+       Room statusChanger = rooms[i];
+       number.editFloorPlan(statusChanger, this); // should update the map of the floor based on each room's status
+     }
+     number.printFloorPlan(this);
+
+   }
+
+
 }
