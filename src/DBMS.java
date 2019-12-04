a71d327c022a2db793ccef1f25924011a530b7f5
Matt McNeive <matt.mcneive@gmail.com>
2019-12-03 18:35:01 -0600

    Updated DBMS

--- src/DBMS.java
+++ src/DBMS.java
@@ -9,44 +9,44 @@ import java.sql.Statement;
 
 
 public class DBMS {
-    Map map = new Map(15);
     public static void main(String[] args) {
-    	System.out.println("Starting");
-          try {
-
-        	  //Connection info for databse
-              String host = "jdbc:mysql://localhost/DoyleHall";	//host address
-              String username = "root";	//username - local host
-              String password = "GroupB171110";	//password
-
-
-              Connection connection = DriverManager.getConnection(host, username, password);
-              System.out.println("Database covh cehjnnected!\n");
-
-              //SQL query
-              Statement stmt = connection.createStatement();
-              //Current table -- faculty
-              String query = "SELECT * FROM faculty";
-              ResultSet rs = stmt.executeQuery(query);
-
-
-              //Reads all info from table made in query
-              while (rs.next()) {
-
-                String last_name = rs.getString("Name");
-                String room = rs.getString("Room_Num");
-                String floor = rs.getString("Room_Floor");
-                String status = rs.getString("Room_Status");
-                System.out.println(last_name + " " + room + " " + status);
-                int r = Integer.parseInt(room);
-                int f = Integer.parseInt(floor);
-                map.setConnection(last_name, r + 1, status, f);
-              }
-
-
-
-          } catch (SQLException err) {
-              System.out.println(err.getMessage());
-          }
-      }
+	    Map map = new Map(15);
+		System.out.println("Starting");
+	      try {
+	
+	    	  //Connection info for databse
+	          String host = "jdbc:mysql://localhost/DoyleHall";	//host address
+	          String username = "root";	//username - local host
+	          String password = "GroupB171110";	//password
+	
+	
+	          Connection connection = DriverManager.getConnection(host, username, password);
+	          System.out.println("Database covh cehjnnected!\n");
+	
+	          //SQL query
+	          Statement stmt = connection.createStatement();
+	          //Current table -- faculty
+	          String query = "SELECT * FROM faculty";
+	          ResultSet rs = stmt.executeQuery(query);
+	
+	
+	          //Reads all info from table made in query
+	          while (rs.next()) {
+	
+	            String last_name = rs.getString("Name");
+	            String room = rs.getString("Room_Num");
+	            String floor = rs.getString("Room_Floor");
+	            String status = rs.getString("Room_Status");
+	            System.out.println(last_name + " " + room + " " + status);
+	            int r = Integer.parseInt(room);
+	            int f = Integer.parseInt(floor);
+	            map.setConnection(last_name, r + 1, status, f);
+	          }
+	
+	
+	
+	      } catch (SQLException err) {
+	          System.out.println(err.getMessage());
+	      }
+	  }
 }
